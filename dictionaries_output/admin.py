c7e314bb877fa58fbd62fa6cee684d552483c191
Jahdie <jahdie@yandex.ru>
2021-06-19 03:57:52 +0700

    Допилил модели и админку. Приступаем к вьюхамю

--- dictionaries_output/admin.py
+++ dictionaries_output/admin.py
@@ -59,10 +59,81 @@ class StationsAdmin(admin.ModelAdmin):
     search_fields = ('pk', 'name')
 
 
+class SwitchCabinetsAdmin(admin.ModelAdmin):
+    def get_hierarchy_note(self, obj):
+        hierarchy_note = obj.hierarchy.note
+        return hierarchy_note
+
+    get_hierarchy_note.short_description = 'Иерархия'
+    list_display = ('pk', 'name', 'note', 'date_created', 'get_hierarchy_note', 'production_id', 'workshop_id',
+                    'compartment_id')
+    list_display_links = ('pk', 'name')
+    search_fields = ('pk', 'name')
+
+
+class ModulesInStationsAdmin(admin.ModelAdmin):
+    def get_hierarchy_note(self, obj):
+        hierarchy_note = obj.hierarchy.note
+        return hierarchy_note
+
+    get_hierarchy_note.short_description = 'Иерархия'
+    list_display = ('pk', 'note', 'date_created', 'get_hierarchy_note', 'module_type_id', 'controller_family_id',
+                    'model_id')
+    list_display_links = ('pk',)
+    search_fields = ('pk',)
+
+
+class SignalsAdmin(admin.ModelAdmin):
+    def get_hierarchy_note(self, obj):
+        hierarchy_note = obj.hierarchy.note
+        return hierarchy_note
+
+    get_hierarchy_note.short_description = 'Иерархия'
+    list_display = ('pk', 'name', 'note', 'date_created', 'get_hierarchy_note', 'marking', 'signal_type_id',
+                    'module_id', 'equipment_type_id', 'equipment_name_id')
+    list_display_links = ('pk',)
+    search_fields = ('pk',)
+
+
+class SwitchPortsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'note', 'date_created', 'port_num', 'is_trunc', 'station_id_or_ports_id', 'switch_id')
+    list_display_links = ('pk',)
+    search_fields = ('pk',)
+
+
+class SwitchesAdmin(admin.ModelAdmin):
+    def get_hierarchy_note(self, obj):
+        hierarchy_note = obj.hierarchy.note
+        return hierarchy_note
+
+    get_hierarchy_note.short_description = 'Иерархия'
+    list_display = ('pk', 'name', 'note', 'date_created', 'get_hierarchy_note', 'ip_adress', 'switch_cabinet_id',
+                    'model_id')
+    list_display_links = ('pk',)
+    search_fields = ('pk',)
+
+
+class SwitchModelsAdmin(admin.ModelAdmin):
+    def get_hierarchy_note(self, obj):
+        hierarchy_note = obj.hierarchy.note
+        return hierarchy_note
+
+    get_hierarchy_note.short_description = 'Иерархия'
+    list_display = ('pk', 'name', 'note', 'date_created', 'get_hierarchy_note', 'ports_num')
+    list_display_links = ('pk', 'name')
+    search_fields = ('pk', 'name')
+
+
+MODEL_LIST = [Productions, EquipmentsNames, EquipmentsTypes, ModulesModels, ModulesTypes, ControllerFamilies, SignalsTypes]
+admin.site.register(MODEL_LIST, DictionariesAdmin)
 admin.site.register(ModelsHierarchy, ModelsHierarchyModelAdmin)
 admin.site.register(Locations, LocationsModelAdmin)
-admin.site.register(Productions, DictionariesAdmin)
 admin.site.register(Workshops, WorkshopsAdmin)
 admin.site.register(Compartments, CompartmentsAdmin)
 admin.site.register(Stations, StationsAdmin)
-admin.site.register(SwitchCabinets)
+admin.site.register(SwitchCabinets, SwitchCabinetsAdmin)
+admin.site.register(ModulesInStations, ModulesInStationsAdmin)
+admin.site.register(Signals, SignalsAdmin)
+admin.site.register(SwitchPorts, SwitchPortsAdmin)
+admin.site.register(SwitchModels, SwitchModelsAdmin)
+admin.site.register(Switches, SwitchesAdmin)
--- dictionaries_output/models.py
+++ dictionaries_output/models.py
@@ -16,6 +16,9 @@ class BaseDictionaryModelAbstract(BaseModelAbstract):
     photo_location = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     hierarchy = models.ForeignKey('ModelsHierarchy', on_delete=models.PROTECT, null=True, verbose_name='Номер иерархии')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         abstract = True
 
@@ -23,15 +26,15 @@ class BaseDictionaryModelAbstract(BaseModelAbstract):
 class ModelsHierarchy(BaseModelAbstract):
     hierarchy_number = models.PositiveIntegerField(blank=True, verbose_name='Номер иерархии')
 
+    def __str__(self):
+        return self.note
+
     class Meta:
         verbose_name = 'Номер иерархии'
         verbose_name_plural = 'Номера иерархии'
 
 
 class Productions(BaseDictionaryModelAbstract):
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Производство'
         verbose_name_plural = 'Производства'
@@ -40,9 +43,6 @@ class Productions(BaseDictionaryModelAbstract):
 class Workshops(BaseDictionaryModelAbstract):
     production_id = models.ForeignKey('Productions', on_delete=models.PROTECT, null=True, verbose_name='Производство',
                                       related_name='production_id')
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'Цех'
@@ -54,24 +54,9 @@ class Compartments(BaseDictionaryModelAbstract):
                                       related_name='production')
     workshop_id = models.ForeignKey('Workshops', on_delete=models.PROTECT, null=True, verbose_name='Цех')
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Участок'
         verbose_name_plural = 'Участки'
-
-
-class SwitchCabinets(BaseDictionaryModelAbstract):
-    location_id = models.ForeignKey('Locations', on_delete=models.PROTECT, null=True,
-                                    verbose_name='Местоположение')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Коммутационный шкаф'
-        verbose_name_plural = 'Коммутационные шкафы'
 
 
 class Stations(BaseDictionaryModelAbstract):
@@ -79,9 +64,6 @@ class Stations(BaseDictionaryModelAbstract):
     production_id = models.ForeignKey('Productions', on_delete=models.PROTECT, null=True, verbose_name='Производство')
     workshop_id = models.ForeignKey('Workshops', on_delete=models.PROTECT, null=True, verbose_name='Цех')
     compartment_id = models.ForeignKey('Compartments', on_delete=models.PROTECT, null=True, verbose_name='Участок')
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'Станция'
@@ -98,3 +80,108 @@ class Locations(BaseModelAbstract):
     class Meta:
         verbose_name = 'Местоположение оборудования'
         verbose_name_plural = 'Местоположения оборудования'
+
+
+class SwitchCabinets(BaseDictionaryModelAbstract):
+    production_id = models.ForeignKey('Productions', on_delete=models.PROTECT, null=True, verbose_name='Производство')
+    workshop_id = models.ForeignKey('Workshops', on_delete=models.PROTECT, null=True, verbose_name='Цех')
+    compartment_id = models.ForeignKey('Compartments', on_delete=models.PROTECT, null=True, verbose_name='Участок')
+
+    class Meta:
+        verbose_name = 'Коммутационный шкаф'
+        verbose_name_plural = 'Коммутационные шкафы'
+
+
+class SwitchModels(BaseDictionaryModelAbstract):
+    ports_num = models.PositiveIntegerField(verbose_name='Количество портов')
+
+    class Meta:
+        verbose_name = 'Модель свитча'
+        verbose_name_plural = 'Модели свитчей'
+
+
+class Switches(BaseDictionaryModelAbstract):
+    ip_adress = models.CharField(max_length=15, verbose_name='IP адресс')
+    switch_cabinet_id = models.ForeignKey('SwitchCabinets', on_delete=models.PROTECT, null=True,
+                                          verbose_name='Коммутационный шкаф')
+    model_id = models.ForeignKey('SwitchModels', on_delete=models.PROTECT, null=True, verbose_name='Модель свитча')
+
+    class Meta:
+        verbose_name = 'Свитч'
+        verbose_name_plural = 'Свитчи'
+
+
+class SwitchPorts(BaseModelAbstract):
+    port_num = models.PositiveIntegerField(verbose_name='Номер порта')
+    # Проверка транк это или хост
+    is_trunc = models.BinaryField(verbose_name='Транк или хост')
+    # Если is_trunc = 0, ставим id станции, если is_trunk = 1, ставим id порта свитча с которым связан этот порт
+    station_id_or_ports_id = models.ForeignKey('Stations', on_delete=models.PROTECT, null=True,
+                                               verbose_name='Станция или порт свитча')
+    switch_id = models.ForeignKey('Switches', on_delete=models.PROTECT, null=True, verbose_name='Свитч')
+
+    class Meta:
+        verbose_name = 'Порт свитча'
+        verbose_name_plural = 'Порты свитчей'
+
+
+class ControllerFamilies(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Семейство контроллеров'
+        verbose_name_plural = 'Семейства контроллеров'
+
+
+class ModulesModels(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Модель модуля'
+        verbose_name_plural = 'Модели модулей'
+
+
+class ModulesTypes(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Тип модуля'
+        verbose_name_plural = 'Типы модулей'
+
+
+class ModulesInStations(BaseModelAbstract):
+    module_type_id = models.ForeignKey('ModulesTypes', on_delete=models.PROTECT, null=True, verbose_name='Тип модуля')
+    controller_family_id = models.ForeignKey('ControllerFamilies', on_delete=models.PROTECT, null=True,
+                                             verbose_name='Семейство контроллеров')
+    model_id = models.ForeignKey('ModulesModels', on_delete=models.PROTECT, null=True, verbose_name='Модель модуля')
+
+    class Meta:
+        verbose_name = 'Модуль в станции'
+        verbose_name_plural = 'Модули в станциях'
+
+
+class SignalsTypes(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Тип сигнала'
+        verbose_name_plural = 'Типы сигналов'
+
+
+class EquipmentsTypes(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Тип оборудования'
+        verbose_name_plural = 'Типы оборудования'
+
+
+class EquipmentsNames(BaseDictionaryModelAbstract):
+    class Meta:
+        verbose_name = 'Наименование оборудования'
+        verbose_name_plural = 'Наименования оборудования'
+
+
+class Signals(BaseDictionaryModelAbstract):
+    marking = models.CharField(max_length=15, verbose_name='Маркировка')
+    signal_type_id = models.ForeignKey('SignalsTypes', on_delete=models.PROTECT, null=True, verbose_name='Тип сигнала')
+    module_id = models.ForeignKey('ModulesInStations', on_delete=models.PROTECT, null=True,
+                                  verbose_name='Модуль в станции')
+    equipment_type_id = models.ForeignKey('EquipmentsTypes', on_delete=models.PROTECT, null=True,
+                                          verbose_name='Тип оборудования')
+    equipment_name_id = models.ForeignKey('EquipmentsNames', on_delete=models.PROTECT, null=True,
+                                          verbose_name='Наименование оборудования')
+
+    class Meta:
+        verbose_name = 'Сигнал'
+        verbose_name_plural = 'Сигналы'
